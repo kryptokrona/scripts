1c2dafc1a708baaad15ecd4388ea2d3b99d79f96
Coffeboi <84473858+Coffeboi@users.noreply.github.com>
2022-12-28 04:48:14 +0100

    Code rework
    Displays data on a table now.

--- xkr-status.py
+++ xkr-status.py
@@ -1,47 +1,48 @@
 import os
-import colorama
-from colorama import Fore
 import requests
 import time
+from prettytable import PrettyTable
+from termcolor import colored
 
 os.system('clear')
 
 def check_site(url):
     start_time = time.time()
-    try:
-        response = requests.get(url)
-        elapsed_time = (time.time() - start_time) * 1000
-        if response.status_code == 200:
-            if elapsed_time > 2000:
-                print((Fore.MAGENTA + f'Host: {url}\n') + (Fore.GREEN + 'Status: OK\n') + (Fore.RED + f'Ping: {elapsed_time: .0f} ms'))
-            elif elapsed_time > 1000:
-                print((Fore.MAGENTA + f'Host: {url}\n') + (Fore.GREEN + 'Status: OK\n') + (Fore.YELLOW + f'Ping: {elapsed_time: .0f} ms'))
-            else:
-                print((Fore.MAGENTA + f'Host: {url}\n') + (Fore.GREEN + 'Status: OK\n') + (Fore.GREEN + f'Ping: {elapsed_time: .0f} ms'))
-        else:
-            print(f'Host: {url} Status: Error {response.status_code}')
-    except requests.RequestException as e:
-        print((Fore.RED + f'An error occurred while sending the request to {url}'))
+    response = requests.get(url)
+    elapsed_time = (time.time() - start_time) * 1000
+    elapsed_time = round(elapsed_time) # Round elapsed time to nearest whole number.
+    if response.status_code == 200:
+        status_color = 'green'
+    else:
+        status_color = 'red'
+    if elapsed_time > 2000: # If elapsed time is over 2000ms, make it red. If it's over 1000ms, make it yellow. Otherwise, leave it white.
+        elapsed_time_color = 'red'
+    elif elapsed_time > 1000:
+        elapsed_time_color = 'yellow'
+    else:
+        elapsed_time_color = 'green'
+    return (colored(url, 'magenta'), colored('OK', status_color), colored(elapsed_time, elapsed_time_color))
 
 def check_node(node):
     start_time = time.time()
-    try:
-        response = requests.get(node)
-        elapsed_time = (time.time() - start_time) * 1000
-        if response.status_code == 200:
-            if elapsed_time > 2000:
-                    node_name = node[7:-8]
-                    print((Fore.BLUE + f'Host: {node_name}\n') + (Fore.RED + 'Status: OK\n') + f'Ping: {elapsed_time: .0f} ms')
-            elif elapsed_time > 1000:
-                     node_name = node[7:-8]
-                     print((Fore.BLUE + f'Host: {node_name}\n') + (Fore.YELLOW + 'Status: OK\n') + f'Ping: {elapsed_time: .0f} ms')
-            else:
-                    node_name = node[7:-8]
-                    print((Fore.BLUE + f'Host: {node_name}\n') + (Fore.GREEN + 'Status: OK\n') + f'Ping: {elapsed_time: .0f} ms')
-        else:
-            print(f'An error occurred for {node}', response.status_code)
-    except requests.RequestException as e:
-        print((Fore.RED + f'An error occurred while sending the request to {node}'))
+    response = requests.get(node)
+    elapsed_time = (time.time() - start_time) * 1000
+    elapsed_time = round(elapsed_time) # Round elapsed time to nearest whole number.
+    if response.status_code == 200:
+        data = response.json()
+        status = data['status']
+        status_color = 'green'
+    else:
+        status = response.status_code
+        status_color = 'red'
+    if elapsed_time > 2000: # If elapsed time is over 2000ms, make it red. If it's over 1000ms, make it yellow. Otherwise, leave it white.
+        elapsed_time_color = 'red'
+    elif elapsed_time > 1000:
+        elapsed_time_color = 'yellow'
+    else:
+        elapsed_time_color = 'green'
+    node_name = node[7:-8]
+    return (colored(node_name, 'blue'), colored(status, status_color), colored(elapsed_time, elapsed_time_color))
 
 # Kryptokrona related sites
 urls = ['https://www.kryptokrona.org', 'https://docs.kryptokrona.org', 'https://explorer.kryptokrona.org', 'https://explorer.kryptokrona.se', 'https://cli.hugin.chat']
@@ -50,16 +51,20 @@ urls = ['https://www.kryptokrona.org', 'https://docs.kryptokrona.org', 'https://
 nodes = ['http://blocksum.org:11898/getinfo', 'http://swepool.org:11898/getinfo', 'http://Tifo.info:11898/getinfo',
     'http://gota.kryptokrona.se:11898/getinfo', 'http://wasa.kryptokrona.se:11898/getinfo',
     'http://spider-pig.hopto.org:11898/getinfo', 'http://privacymine.net:11898/getinfo', 'http://privacymine.net:21898/getinfo',
-    'http://techy.ddns.net:11898/getinfo', 'http://106.12.131.174:11898/getinfo',
-    'http://norpool.org:11898/getinfo', 'http://115.239.210.250:11898/getinfo', 'http://182.43.80.115:11898/getinfo']
+    'http://106.12.131.174:11898/getinfo', 'http://norpool.org:11898/getinfo', 'http://115.239.210.250:11898/getinfo', 'http://182.43.80.115:11898/getinfo']
 
 def main():
-    print('Kryptokrona related sites:')
+    # Create a table with columns for URL, status, and elapsed time
+    table = PrettyTable(['URL/Node', 'Status', 'Elapsed Time (ms)'])
+
     for url in urls:
-        check_site(url)
-    print('')
-    print('Kryptokrona nodes:')
+        result = check_site(url)
+        table.add_row(result)
+
     for node in nodes:
-        check_node(node)
+        result = check_node(node)
+        table.add_row(result)
+
+    print(table)
 
 main()
